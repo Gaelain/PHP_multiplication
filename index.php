73cb3e42b1cb4f9c2ba232c96685a0c4e2252256
Meliodache <ache300@hotmail.fr>
2019-09-19 15:33:58 +0200

    Ajout de boucle pour l'affichage de l'index

--- index.php
+++ index.php
@@ -8,15 +8,12 @@
     </head>
     <body>
         <h1>Table de multiplication :</h1>
-        <p><a href="table.php?t=1">Table de 1</a></p>
-        <p><a href="table.php?t=2">Table de 2</a></p>
-        <p><a href="table.php?t=3">Table de 3</a></p>
-        <p><a href="table.php?t=4">Table de 4</a></p>
-        <p><a href="table.php?t=5">Table de 5</a></p>
-        <p><a href="table.php?t=6">Table de 6</a></p>
-        <p><a href="table.php?t=7">Table de 7</a></p>
-        <p><a href="table.php?t=8">Table de 8</a></p>
-        <p><a href="table.php?t=9">Table de 9</a></p>
-        <p><a href="table.php?t=10">Table de 10</a></p>
+        <?php for($i=1; $i <=10;$i++)
+        {
+        ?> 
+            <p><a href="table.php?t= <?php echo $i ?>" >Table de <?php echo $i ?></a></p>
+            <?php
+        }
+        ?>
     </body>
 </html>
